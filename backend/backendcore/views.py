af8f6221cde421325ffb9b5925f2664c1e206301
DorianWlodarczyk <215365@edu.p.lodz.pl>
2023-03-09 18:24:37 +0100

    Backend init and CORS setup

---
+++ backend/backendcore/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttPResponse
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+
+# Create your views here.
