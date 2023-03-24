415a441250da093dc0fa2543487e98791f6e8d9a
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-03-24 20:53:56 +0100

    Add sign in page content from tutorial w/ updated event handling

---
+++ frontend/lib/screens/sign_in/bloc/sign_in_bloc.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:workout_app/core/service/auth_service.dart';
+import 'package:workout_app/core/service/validation_service.dart';
+import 'package:flutter/material.dart';
+
+part 'sign_in_event.dart';
+part 'sign_in_state.dart';
+
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool isButtonEnabled = false;
+
+  SignInBloc() : super(SignInInitial()) {
+    on<OnTextChangeEvent>(onTextChange);
+    on<SignInTappedEvent>(onSignInTapped);
+    on<ForgotPasswordTappedEvent>((event, emit) => emit(NextForgotPasswordPageState()));
+    on<SignUpTappedEvent>((event, emit) => emit(NextSignUpPageState()));
+  }
+
+  Future<void> onTextChange(
+    OnTextChangeEvent event,
+    Emitter<SignInState> emit
+  ) async {
+      if (isButtonEnabled != _checkIfSignInButtonEnabled()) {
+        isButtonEnabled = _checkIfSignInButtonEnabled();
+        emit(SignInButtonEnableChangedState(isEnabled: isButtonEnabled));
+      }
+  }
+
+  Future<void> onSignInTapped(
+    SignInTappedEvent event, 
+    Emitter<SignInState> emit
+  ) async {
+      if (_checkValidatorsOfTextField()) {
+        try {
+          emit(LoadingState());
+          await AuthService.signIn(emailController.text, passwordController.text);
+          emit(NextTabBarPageState());
+          print("Go to the next page");
+        } catch (e) {
+          print('E to tstrng: ' + e.toString());
+          emit(ErrorState(message: e.toString()));
+        }
+      } else {
+        emit(ShowErrorState());
+      }
+  }
+  
+  bool _checkIfSignInButtonEnabled() {
+    return emailController.text.isNotEmpty && passwordController.text.isNotEmpty;
+  }
+
+  bool _checkValidatorsOfTextField() {
+    return ValidationService.email(emailController.text) && ValidationService.password(passwordController.text);
+  }
+}
