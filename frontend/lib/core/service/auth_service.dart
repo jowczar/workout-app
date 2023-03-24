7d193e575b71eac5443fa2ffe0698b37912d372e
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-03-24 20:03:32 +0100

    Add example services

---
+++ frontend/lib/core/service/auth_service.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+// TODO: add here requests to backend 
+
+enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+
+class AuthService {
+  static final _controller = StreamController<AuthenticationStatus>();
+
+  static Stream<AuthenticationStatus> get status async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus.unauthenticated;
+    yield* _controller.stream;
+  }
+
+  static Future<void> signIn(String username, String password) async {
+    await Future.delayed(
+      const Duration(milliseconds: 300),
+      () => _controller.add(AuthenticationStatus.authenticated),
+    );
+  }
+
+  static void signOut() {
+    _controller.add(AuthenticationStatus.unauthenticated);
+  }
+
+  void dispose() => _controller.close();
+}
