df65292cabfedd16f9e3a7b153c9289f6cc18ad4
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-03-24 20:06:33 +0100

    Add common widgets

---
+++ frontend/lib/screens/common_widgets/button.dart
@@ -0,0 +1,39 @@
+import 'package:workout_app/core/const/color_constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final bool isEnabled;
+  final Function() onTap;
+
+  CustomButton({required this.title, this.isEnabled = true, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 55,
+      decoration: BoxDecoration(
+        color: isEnabled ? ColorConstants.primaryColor : ColorConstants.disabledColor,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(100),
+          onTap: onTap,
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                color: ColorConstants.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/screens/common_widgets/text_field.dart
@@ -0,0 +1,191 @@
+import 'package:workout_app/core/const/color_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String title;
+  final String placeholder;
+  final String errorText;
+  final bool obscureText;
+  final bool isError;
+  final TextEditingController controller;
+  final VoidCallback onTextChanged;
+  final TextInputAction textInputAction;
+  final TextInputType? keyboardType;
+
+  const CustomTextField({
+    required this.title,
+    required this.placeholder,
+    this.obscureText = false,
+    this.isError = false,
+    required this.controller,
+    required this.onTextChanged,
+    required this.errorText,
+    this.textInputAction = TextInputAction.done,
+    this.keyboardType,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  final focusNode = FocusNode();
+  bool stateObscureText = false;
+  bool stateIsError = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    focusNode.addListener(
+      () {
+        setState(() {
+          if (focusNode.hasFocus) {
+            stateIsError = false;
+          }
+        });
+      },
+    );
+
+    stateObscureText = widget.obscureText;
+    stateIsError = widget.isError;
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomTextField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    stateObscureText = widget.obscureText;
+    stateIsError = focusNode.hasFocus ? false : widget.isError;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _createHeader(),
+          const SizedBox(height: 5),
+          _createTextFieldStack(),
+          if (stateIsError) ...[
+            _createError(),
+          ],
+        ],
+      ),
+    );
+  }
+
+  Widget _createHeader() {
+    return Text(
+      widget.title,
+      style: TextStyle(
+        color: _getUserNameColor(),
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Color _getUserNameColor() {
+    if (focusNode.hasFocus) {
+      return ColorConstants.primaryColor;
+    } else if (stateIsError) {
+      return ColorConstants.errorColor;
+    } else if (widget.controller.text.isNotEmpty) {
+      return ColorConstants.textBlack;
+    }
+    return ColorConstants.grey;
+  }
+
+  Widget _createTextFieldStack() {
+    return Stack(
+      children: [
+        _createTextField(),
+        // TODO: I had to hide obscuretext functionality because we have no icon for this, 
+        // feel free to remove this code or add our icon when the design is finished
+        // (this code comes from tutorial)
+        // if (widget.obscureText) ...[
+        //   Positioned(
+        //     right: 0,
+        //     bottom: 0,
+        //     top: 0,
+        //     child: _createShowEye(),
+        //   ),
+        // ],
+      ],
+    );
+  }
+
+  Widget _createTextField() {
+    return TextField(
+      focusNode: focusNode,
+      controller: widget.controller,
+      obscureText: stateObscureText,
+      textInputAction: widget.textInputAction,
+      keyboardType: widget.keyboardType,
+      style: TextStyle(
+        color: ColorConstants.textBlack,
+        fontSize: 16,
+      ),
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide(
+            color: stateIsError ? ColorConstants.errorColor : ColorConstants.textFieldBorder.withOpacity(0.4),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide(
+            color: ColorConstants.primaryColor,
+          ),
+        ),
+        hintText: widget.placeholder,
+        hintStyle: TextStyle(
+          color: ColorConstants.grey,
+          fontSize: 16,
+        ),
+        filled: true,
+        fillColor: ColorConstants.textFieldBackground,
+      ),
+      onChanged: (text) {
+        setState(() {});
+        widget.onTextChanged();
+      },
+    );
+  }
+
+  // Widget _createShowEye() {
+  //   return GestureDetector(
+  //     onTap: () {
+  //       setState(() {
+  //         stateObscureText = !stateObscureText;
+  //       });
+  //     },
+  //     child: Image(
+  //       image: AssetImage(
+  //         // PathConstants.eye,
+  //       ),
+  //       color: widget.controller.text.isNotEmpty ? ColorConstants.primaryColor : ColorConstants.grey,
+  //     ),
+  //   );
+  // }
+
+  Widget _createError() {
+    return Container(
+      padding: const EdgeInsets.only(top: 2),
+      child: Text(
+        widget.errorText,
+        style: TextStyle(
+          fontSize: 14,
+          color: ColorConstants.errorColor,
+        ),
+      ),
+    );
+  }
+}
