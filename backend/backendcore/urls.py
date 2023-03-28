3e28f700ca501d2464309f91d6ab2c0e75ce7e02
DorianWlodarczyk <215365@edu.p.lodz.pl>
2023-03-28 11:13:07 +0200

    added registration templates for testing backend

--- backend/backendcore/urls.py
+++ backend/backendcore/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('hellodjango', views.hello_django, name ='hellodjango'),
     path('home', views.home),
+    path('', views.signIn),
     path('postsignIn/', views.postsignIn),
     path('signUp/', views.signUp, name="signup"),
     path('logout/', views.logout, name="log"),
--- backend/backendcore/views.py
+++ backend/backendcore/views.py
@@ -33,3 +33,46 @@ def home(request):
 def hello_django(request):
     print("Request received!")
     return Response({'message:Request successfully returned! Hello Django'}, status= 200)
+
+def signIn(request):
+    return render(request,"Login.html")
+    
+def home(request):
+    return render(request,"Home.html")
+ 
+def postsignIn(request):
+    email=request.POST.get('email')
+    pasw=request.POST.get('pass')
+    try:
+        # if there is no error then signin the user with given email and password
+        user=authe.sign_in_with_email_and_password(email,pasw)
+    except:
+        message="Invalid Credentials!!Please ChecK your Data"
+        return render(request,"Login.html",{"message":message})
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request,"Home.html",{"email":email})
+ 
+def logout(request):
+    try:
+        del request.session['uid']
+    except:
+        pass
+    return render(request,"Login.html")
+ 
+def signUp(request):
+    return render(request,"Registration.html")
+ 
+def postsignUp(request):
+     email = request.POST.get('email')
+     passs = request.POST.get('pass')
+     name = request.POST.get('name')
+     try:
+        # creating a user with the given email and password
+        user=authe.create_user_with_email_and_password(email,passs)
+        uid = user['localId']
+        idtoken = request.session['uid']
+        print(uid)
+     except:
+        return render(request, "Registration.html")
+     return render(request,"Login.html")
