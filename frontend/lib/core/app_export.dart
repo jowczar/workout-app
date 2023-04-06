5c351a0cc2b20b63d683b2fb6ebc40cfab2d4365
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-04-06 17:21:29 +0200

    Add svg & image provider

--- frontend/lib/core/app_export.dart
+++ frontend/lib/core/app_export.dart
@@ -3,4 +3,4 @@ export 'package:workout_app/core/utils/text_constant.dart';
 export 'package:workout_app/core/utils/image_constant.dart';
 export 'package:workout_app/core/utils/size_utils.dart';
 export 'package:workout_app/routes/app_routes.dart';
-export 'package:workout_app/core/utils/date_time_utils.dart';
+export 'package:workout_app/screens/common_widgets/custom_image_view.dart';
