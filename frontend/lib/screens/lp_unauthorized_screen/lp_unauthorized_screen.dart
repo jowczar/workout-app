ae025ce1051f4d1c29e08e5c25df31ab729a3852
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-04-11 18:00:03 +0200

    Fix unsupported logo svg property

--- frontend/lib/screens/lp_unauthorized_screen/lp_unauthorized_screen.dart
+++ frontend/lib/screens/lp_unauthorized_screen/lp_unauthorized_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:workout_app/core/app_export.dart';
 
 class LpUnauthorizedScreen extends StatelessWidget {
@@ -12,91 +13,15 @@ class LpUnauthorizedScreen extends StatelessWidget {
         body: Container(
           width: size.width,
           height: size.height,
-          decoration: BoxDecoration(
-            color: ColorConstant.backgroundColor,
-            image: DecorationImage(
-              image: AssetImage(
-                ImageConstant.imgLogo,
-              ),
-              fit: BoxFit.cover,
-            ),
-          ),
           child: Container(
             width: double.maxFinite,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  height: getVerticalSize(
-                    402,
-                  ),
-                  width: double.maxFinite,
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      CustomImageView(
-                        imagePath: ImageConstant.imgActivity,
-                        height: getVerticalSize(
-                          359,
-                        ),
-                        width: getHorizontalSize(
-                          360,
-                        ),
-                        alignment: Alignment.topCenter,
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          height: getVerticalSize(
-                            57,
-                          ),
-                          width: getHorizontalSize(
-                            243,
-                          ),
-                          margin: getMargin(
-                            bottom: 37,
-                          ),
-                          child: Stack(
-                            alignment: Alignment.bottomCenter,
-                            children: [
-                              Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  "ANCIENT",
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.left,
-                                  style: AppStyle.txtUltraRegular44,
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Padding(
-                                  padding: getPadding(
-                                    bottom: 5,
-                                  ),
-                                  child: SizedBox(
-                                    width: getHorizontalSize(
-                                      233,
-                                    ),
-                                    child: Divider(
-                                      height: getVerticalSize(
-                                        1,
-                                      ),
-                                      thickness: getVerticalSize(
-                                        1,
-                                      ),
-                                      color: ColorConstant.whiteA700,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset(ImageConstant.imgLogo)
                 ),
                 Container(
                   width: getHorizontalSize(
