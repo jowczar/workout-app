152f94c99ce5bef20d8b9bdd4ef60c1480bf7e1b
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-04-14 15:58:14 +0200

    Fix generated code

--- frontend/lib/screens/lp_unauthorized_screen/lp_unauthorized_screen.dart
+++ frontend/lib/screens/lp_unauthorized_screen/lp_unauthorized_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:workout_app/core/app_export.dart';
+import 'package:workout_app/screens/common_widgets/custom_button.dart';
 
 class LpUnauthorizedScreen extends StatelessWidget {
   @override
@@ -20,82 +21,26 @@ class LpUnauthorizedScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(20.0),
+                  padding: const EdgeInsets.all(30.0),
                   child: Image.asset(ImageConstant.imgLogo)
                 ),
-                Container(
-                  width: getHorizontalSize(
-                    234,
-                  ),
-                  margin: getMargin(
-                    top: 31,
-                  ),
-                  padding: getPadding(
-                    left: 30,
-                    top: 1,
-                    right: 84,
-                    bottom: 1,
-                  ),
-                  decoration: AppDecoration.txtOutlineBluegray500.copyWith(
-                    borderRadius: BorderRadiusStyle.txtCircleBorder24,
-                  ),
-                  child: Text(
-                    "Sign in",
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.left,
-                    style: AppStyle.txtTekoRegular32Black900,
-                  ),
+                CustomButton(
+                  onTap: () {
+                    // AppRoutes.lpLoginScreen
+                    // Navigator.pushNamed(context, );
+                  },
+                  text: "Sign in",
+                  width: 238
                 ),
-                Container(
-                  height: getVerticalSize(
-                    46,
-                  ),
-                  width: getHorizontalSize(
-                    234,
-                  ),
-                  margin: getMargin(
-                    top: 16,
-                    bottom: 5,
-                  ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          height: getVerticalSize(
-                            44,
-                          ),
-                          width: getHorizontalSize(
-                            234,
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                              getHorizontalSize(
-                                22,
-                              ),
-                            ),
-                            border: Border.all(
-                              color: ColorConstant.blueGray500,
-                              width: getHorizontalSize(
-                                2,
-                              ),
-                              strokeAlign: strokeAlignOutside,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Sign up",
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.left,
-                          style: AppStyle.txtTekoRegular32,
-                        ),
-                      ),
-                    ],
-                  ),
+                SizedBox(height: 16),
+                CustomButton(
+                  onTap: () {
+                    // AppRoutes.lpSignUpScreen
+                    // Navigator.pushNamed(context, );
+                  },
+                  text: "Sign up",
+                  width: 238,
+                  variant: ButtonVariant.Secondary
                 ),
               ],
             ),
