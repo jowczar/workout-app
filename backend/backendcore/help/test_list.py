37e3a8e047a2e8143adab6fe58b91102771b09be
Czarek Karczewski <keracz1997@gmail.com>
2023-04-14 14:59:17 +0200

    add test file with dummy plans

---
+++ backend/backendcore/help/test_list.py
@@ -0,0 +1,109 @@
+plans1 = {
+    'exercise_list': {
+        'dead_lift': {
+            'set1' : {
+                'weight': 300,
+                'reps' : 3
+            },
+            'set2' : {
+                'weight': 300,
+                'reps' : 3
+            },
+            'set3' : {
+                'weight': 350,
+                'reps' : 2
+            }
+        },
+        'squat' : {
+            'set1' : {
+                'weight': 300,
+            'reps' : 3
+            },
+            'set1' : {
+                'weight': 300,
+            'reps' : 1
+            }
+        },
+        'run': {
+            'distance' : 3,
+            'time' : 10
+        }
+    }
+}
+    
+plans2 = {
+    'exercise_list': {
+        'ohp': {
+            'set1' : {
+                'weight': 30,
+            'reps' : 3
+            },
+            'set2' : {
+                'weight': 50,
+                'reps' : 3
+            },
+            'set3' : {
+                'weight': 70,
+                'reps' : 2
+            }
+        },
+        'pull_ups' : {
+            'set1' : {
+                'weight': 4,
+            'reps' : 3
+            },
+            'set1' : {
+                'weight': 7,
+            'reps' : 1
+            }
+        },
+        'bike': {
+            'distance' : 3,
+            'time' : 10
+        }
+    }
+}
+
+plans3 = {
+    'exercise_list': {
+        'biceps': {
+            'set1' : {
+                'weight': 10,
+                'reps' : 11
+            },
+            'set2' : {
+                'weight': 10,
+                'reps' : 12
+            },
+            'set3' : {
+                'weight': 15,
+                'reps' : 15
+            }
+        },
+        'push_ups' : {
+            'set1' : {
+                'weight': 100,
+                'reps' : 30
+            },
+            'set1' : {
+                'weight': 100,
+                'reps' : 30
+            }
+        },
+        'run': {
+            'distance' : 10,
+            'time' : 60
+        }
+    }
+}
+
+plans4 = {
+    'exercise_list': {
+        'biceps': {
+        },
+        'push_ups' : {
+        },
+        ' ': {
+        }
+    }
+}
