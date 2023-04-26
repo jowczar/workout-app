b75c24f9e506f1a30e3cc76b101bcd64223e3721
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2023-04-27 01:10:54 +0200

    Add new approach to slidable button

--- frontend/lib/screens/common_widgets/slidable_button.dart
+++ frontend/lib/screens/common_widgets/slidable_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:indexed/indexed.dart';
 import 'package:workout_app/core/app_export.dart';
 import 'package:workout_app/screens/common_widgets/custom_button.dart';
 
@@ -9,23 +10,41 @@ class SlidableButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Slidable(
-            endActionPane: ActionPane(motion: StretchMotion(), children: [
+    return SafeArea(
+        child: SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(children: <Widget>[
+        Slidable(
+          endActionPane: ActionPane(
+            motion: StretchMotion(),
+            extentRatio: 0.4,
+            children: [
               SlidableAction(
-                onPressed: ((context) {
-                  // delete something
-                }),
-                icon: Icons.delete,
-                backgroundColor: Colors.transparent,
-              )
-            ]),
-            child: CustomButton(
-                onTap: () {
-                  //do something here
-                },
-                text: TextConstant.deadlift,
-                width: 400,
-                variant: ButtonVariant.Secondary)));
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                  onPressed: ((context) {
+                    // delete something
+                  }),
+                  icon: Icons.delete,
+                  foregroundColor: Color.fromARGB(255, 233, 120, 112),
+                  backgroundColor: Colors.transparent,
+                  padding: EdgeInsets.all(10),
+                  spacing: 8,
+                  label: "Usun")
+            ],
+          ),
+          child: CustomButton(
+              onTap: () {
+                //do something here
+              },
+              text: TextConstant.deadlift,
+              width: 400,
+              variant: ButtonVariant.Secondary),
+        ),
+      ]),
+    ));
   }
 }
